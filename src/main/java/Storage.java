a085cffec1aaa8dda2e92abb244bfe6512b84b9a
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 17:35:29 +0300

    Scripted auto-commit on change (2019-12-27 17:35:29) by gitwatch.sh

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,27 +1,53 @@
-import org.zeromq.ZContext;
 import org.zeromq.*;
 import org.zeromq.ZMQ.Socket;
-import zmq.poll.Poller;
 
 import java.util.Scanner;
 
 public class Storage {
 
-    private final static String data = "abcdefghklmnoprstufxasldhbasldhbasdaslhbasfadvldashbdashgdvasljdvjasdbasdal";
+    private final static String mainData = "abcdefghklmnoprstufxasldhbasldhbasdaslhbasfadvldashbdashgdvasljdvjasdbasdal";
+    private static int left;
+    private static int right;
+    private static Socket responder;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int left = in.nextInt();
-        int right = in.nextInt();
+        left = in.nextInt();
+        right = in.nextInt();
 
+        if (left > right || left > mainData.length() - 1 || right > mainData.length() - 1) {
+            System.out.println("wrond borders");
+        }
+
+        String data = mainData.substring(left, right);
 
         ZMQ.Context context = ZMQ.context(1);
-        Socket responder = context.socket(SocketType.DEALER);
+        responder = context.socket(SocketType.DEALER);
         responder.connect("tcp://localhost:5560");
+        long start = System.currentTimeMillis();
+
+
+        putMessageTogetherAndSend("NEW");
 
         while (!Thread.currentThread().isInterrupted()) {
 
+            if (System.currentTimeMillis() - start > 5000) {
+                start = System.currentTimeMillis();
+                putMessageTogetherAndSend("I STILL ALIVE");
+            }
+
+            ZMsg msgReceive = ZMsg.recvMsg(responder);
+
+
+
         }
 
+    }
+
+    private static void putMessageTogetherAndSend(String frame) {
+        ZMsg msg = new ZMsg();
+        msg.add(frame);
+        msg.add(left + "//" + right);
+        msg.send(responder);
     }
 }
