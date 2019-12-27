a041a27813fc8e522f13e73693a7190abfec6b57
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 17:04:17 +0300

    Scripted auto-commit on change (2019-12-27 17:04:17) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,9 +1,20 @@
 import org.zeromq.ZContext;
 import org.zeromq.*;
 import org.zeromq.ZMQ.Socket;
+import zmq.poll.Poller;
 
 public class Server {
     public static void main(String[] args) {
-        
+        ZContext context = new ZContext();
+
+        Socket frontend = context.createSocket(SocketType.ROUTER);
+        Socket backend = context.createSocket(SocketType.ROUTER);
+
+        ZMQ.Poller items = context.createPoller(2);
+        items.register(frontend, ZMQ.Poller.POLLIN);
+        items.register(backend, ZMQ.Poller.POLLIN);
+
+
+
     }
 }
