f6cf9d13cde4ed5330acd628ee668a8de8345a05
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 17:23:36 +0300

    Scripted auto-commit on change (2019-12-27 17:23:36) by gitwatch.sh

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,2 +1,27 @@
+import org.zeromq.ZContext;
+import org.zeromq.*;
+import org.zeromq.ZMQ.Socket;
+import zmq.poll.Poller;
+
+import java.util.Scanner;
+
 public class Storage {
+
+    private final static String data = "abcdefghklmnoprstufxasldhbasldhbasdaslhbasfadvldashbdashgdvasljdvjasdbasdal";
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int left = in.nextInt();
+        int right = in.nextInt();
+
+
+        ZMQ.Context context = ZMQ.context(1);
+        Socket responder = context.socket(SocketType.DEALER);
+        responder.connect("tcp://localhost:5560");
+
+        while (!Thread.currentThread().isInterrupted()) {
+
+        }
+
+    }
 }
