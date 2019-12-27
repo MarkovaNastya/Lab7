9e085dd31ec96d53dcd270a156e20b4d18b08dcc
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 18:00:59 +0300

    Scripted auto-commit on change (2019-12-27 18:00:59) by gitwatch.sh

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -10,15 +10,17 @@ import java.util.Map;
 public class Server {
 
     private static HashMap<Pair<Integer, Integer>, Pair<ZFrame, Long>> storages = new HashMap<>();
+    private static Socket frontend;
+    private static Socket backend;
 
     public static void main(String[] args) {
 
         ZMQ.Context context = ZMQ.context(1);
 
-        Socket frontend = context.socket(SocketType.ROUTER);
+        frontend = context.socket(SocketType.ROUTER);
         frontend.bind("tcp://localhost:5559");
 
-        Socket backend = context.socket(SocketType.ROUTER);
+        backend = context.socket(SocketType.ROUTER);
         backend.bind("tcp://localhost:5560");
 
         ZMQ.Poller items = context.poller(2);
@@ -39,7 +41,7 @@ public class Server {
                     System.out.println(message);
 
                     ZFrame adress = message.pop();
-                    ZFrame nul = message.pop();
+                    ZFrame nullFrame = message.pop();
 
                     String command = message.popString();
 
@@ -47,15 +49,16 @@ public class Server {
                         int index = Integer.parseInt(message.popString());
 
                         for (Map.Entry<Pair<Integer, Integer>, Pair<ZFrame, Long>> storage : storages.entrySet()) {
+                            if (index >= storage.getKey().getKey() && index < storage.getKey().getValue()) {
 
+                            }
                         }
 
                     } else if (command.equals("SET")) {
 
                         int index = Integer.parseInt(message.popString());
                         String elem = message.popString();
-                        System.out.println(index);
-                        System.out.println(elem);
+
 
 
                     }
@@ -114,5 +117,14 @@ public class Server {
 
 
 
+    }
+
+
+    private static void putMessageTogetherAndSendToBackend(String frame) {
+        ZMsg msg = new ZMsg();
+
+        
+
+        msg.send(backend);
     }
 }
