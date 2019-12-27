689774fed1d70850bd3d322502339fd7078d3c70
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 17:12:23 +0300

    Scripted auto-commit on change (2019-12-27 17:12:23) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,15 @@
+import org.zeromq.ZContext;
+import org.zeromq.*;
+import org.zeromq.ZMQ.Socket;
+import zmq.poll.Poller;
+
+
+
 public class Client {
+    public static void main(String[] args) {
+
+        ZMQ.Context context = ZMQ.context(1);
+        Socket responder 
+
+    }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,7 +6,6 @@ import zmq.poll.Poller;
 public class Server {
     public static void main(String[] args) {
 
-
         ZMQ.Context context = ZMQ.context(1);
 
         Socket frontend = context.socket(SocketType.ROUTER);
@@ -14,12 +13,43 @@ public class Server {
 
         Socket backend = context.socket(SocketType.ROUTER);
         backend.bind("tcp://localhost:5559");
-        
+
         ZMQ.Poller items = context.poller(2);
         items.register(frontend, ZMQ.Poller.POLLIN);
         items.register(backend, ZMQ.Poller.POLLIN);
 
         boolean more = false;
+        byte[] message;
+
+        while (!Thread.currentThread().isInterrupted()) {
+            items.poll();
+
+            if (items.pollin(0)) {
+                while (true) {
+                    message = frontend.recv(0);
+                    more = frontend.hasReceiveMore();
+
+
+                    if (!more) {
+                        break;
+                    }
+                }
+
+            }
+
+            if (items.pollin(1)) {
+                message = backend.recv(0);
+                more = backend.hasReceiveMore();
+
+
+                if (!more) {
+                    break;
+                }
+
+            }
+
+
+        }
 
 
 
