f49fda58dd87e1ab3d5949376c2c5c00547eddb7
GromkovAlexander <gromkovalex@mail.ru>
2019-12-27 17:16:44 +0300

    Scripted auto-commit on change (2019-12-27 17:16:44) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,13 +3,39 @@ import org.zeromq.*;
 import org.zeromq.ZMQ.Socket;
 import zmq.poll.Poller;
 
+import java.util.Scanner;
 
 
 public class Client {
     public static void main(String[] args) {
 
         ZMQ.Context context = ZMQ.context(1);
-        Socket responder 
+        Socket responder = context.socket(SocketType.REQ);
+        responder.connect("tcp://localhost:5559");
+
+        Scanner in = new Scanner(System.in);
+
+        while (!Thread.currentThread().isInterrupted()) {
+
+            String command = in.nextLine();
+
+            if (command.startsWith("/STOP")) {
+                break;
+            }
+
+            String[] commandSplit = command.split(" ");
+
+            if (commandSplit[0].equals("GET")) {
+                ZMsg msg = new ZMsg();
+                msg.add()
+            }
+
+
+            if (commandSplit[0].equals("SET")) {
+
+            }
+
+        }
 
     }
 }
